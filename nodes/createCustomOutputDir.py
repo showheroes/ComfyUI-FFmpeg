6fdaf92bd94674d500d40f8bf60ee8c0876286b6
evgeny-l <lexunin@gmail.com>
2025-06-05 16:05:48 +0200

    Create custom output dir node.

---
+++ nodes/createCustomOutputDir.py
@@ -0,0 +1,35 @@
+import json
+import math
+import os
+import subprocess
+from comfy.comfy_types.node_typing import IO
+
+class CreateCustomOutputDir:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": { 
+                "dir_name": (IO.STRING, {"default": "project_123", }),
+                "timestamp_postfix": (IO.BOOLEAN, {"default": True}),
+            },
+        }
+
+    RETURN_TYPES = (IO.STRING,)
+    RETURN_NAMES = ("output_path")
+    FUNCTION = "createCustomOutputDir"
+    OUTPUT_NODE = True
+    CATEGORY = "🔥FFmpeg"
+  
+    def createCustomOutputDir(self, dir_name, timestamp_postfix):
+        try:
+            output_path = os.path.join(comfy_paths.get_output_directory(), dir_name)
+            if timestamp_postfix:
+                output_path = os.path.join(output_path, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+            os.makedirs(output_path, exist_ok=True)
+            logging.info(f"[ FFMpeg Utils ] Makedir: {output_path}")
+            return output_path,
+        except Exception as e:
+            raise ValueError(e)
--- nodes_map.py
+++ nodes_map.py
@@ -16,6 +16,7 @@ from .nodes.addAudio import *
 from .nodes.imagesSave import *
 from .nodes.pipVideo import *
 from .nodes.videoTransition import *
+from .nodes.createCustomOutputDir import *
 
 NODE_CLASS_MAPPINGS = {
     "Video2Frames": Video2Frames,
@@ -36,6 +37,7 @@ NODE_CLASS_MAPPINGS = {
     "ImagesSave": ImagesSave,
     "PipVideo": PipVideo,
     "VideoTransition": VideoTransition,
+    "CreateCustomOutputDir": CreateCustomOutputDir,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -58,4 +60,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImagesSave": "🔥ImagesSave",
     "PipVideo": "🔥PipVideo",
     "VideoTransition": "🔥VideoTransition",
+    "CreateCustomOutputDir": "🔥CreateCustomOutputDir",
 }
